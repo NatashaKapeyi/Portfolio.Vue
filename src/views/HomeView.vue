f74381790bf88242b097d59f39b7d9b59265072d
NatashaKapeyi <natashazikhona7@gmail.com>
2024-01-14 04:08:17 +0200

    done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,13 @@
 <template >
-  <div class="container ">
+  <div class="container pt-5">
     <div class="row">
       <div class="col">
       <h1>NK</h1> 
       <h2 class="bg-white align-top">PORTFOLIO</h2>
       <h3>Natasha Kapeyi</h3>
       <h4 class="display-5">A <span></span>Fullstack Developer</h4>
-      <p class="display-6"><a class="text-black" href="https://drive.google.com/file/d/1JapHX95W_VrVnlbtsBC08JCz3lKhJ19j/view" download target="_blank" ><i class="bi bi-file-earmark-arrow-down"></i> </a>Carriculum Vitae</p>
+      <p class="display-6"><a class="text-black" href="https://drive.google.com/file/d/1JapHX95W_VrVnlbtsBC08JCz3lKhJ19j/view" download target="_blank" ><i class="bi bi-file-earmark-arrow-down "></i></a>Carriculum Vitae
+      </p>
      </div>
    </div>
  </div>
@@ -66,8 +67,9 @@ h1,h2,h3{
 
 }
 
-h1,h2,h3,h4 {
-  overflow: hidden;
+h1,h2,h3,h4,p {
+overflow-x: visible;
+overflow-y: hidden;
   animation: typing 3s backwards;
   white-space: nowrap;
 }
@@ -84,9 +86,10 @@ h1,h2,h3,h4 {
   content: url(https://i.ibb.co/rm9q8Bk/IMG-20200822-WA0045-removebg-preview.png);
   background-repeat: no-repeat;
   position: absolute;
-    top: 30.3rem;
+    top: 33.25rem;
    right:-6rem;
    flex-shrink: 0;
+  
 }
 
 
@@ -95,5 +98,64 @@ i{
   cursor:grab;
 }
 
+@media screen and (max-width: 1000px) {
+  *{
+    overflow-y: hidden;
+   
+  }
 
+  
+
+  .row,.col,h1,h3,.container::before,.container{
+    max-width: max-content;
+    
+    
+  }
+
+   
+  /* .col{
+     
+    padding: 0;
+    margin: 1px 1px 3px ;
+    
+  } */
+  h1{
+     
+    width: fit-content;
+    font-size: 181px; 
+  
+  }
+
+  h2{
+  font-size: 46px;
+  bottom: -55px;
+  margin-bottom: 100px;
+  }
+
+  h3{
+     
+    font-size: 39px;
+  }
+
+  
+  .container::before{
+    display: contents;
+   
+  }
+   
+  .container{
+    display: flex;
+    flex-direction: column-reverse;
+    flex-wrap: wrap;
+     align-content: space-between;
+  }
+
+  .display-6{
+    text-align: start;
+  }
+    
+    }
+
+        
+   
 </style>
